eec8e2d6d7efea16735ece8560995f49e96891df
Zeiin <Ztalkyn@gmail.com>
2024-10-30 12:37:48 +0500

    fixed

---
+++ homeworks/30.10/const.js
@@ -0,0 +1,11 @@
+// var name = "Aty";
+
+// let surname = "Familia";
+
+// surname = "ABC";
+
+// const id = 909009;
+
+// id = 84848484;
+
+// console.log(id);
---
+++ homeworks/30.10/script.js
@@ -0,0 +1,52 @@
+// let season = 'Fall';
+
+// console.log(season === 'Fall');
+
+// let name = "Aziza"
+// let day = 30; 
+
+// let grades = [3, 4, 5, 6, 7, 10] ;
+
+
+
+// let name = "Askhat";
+
+// let surname = "Abutolib";
+
+// let age = 64; 
+
+// let weight = 70.45; 
+
+// let streetName = "Maulenova 92";
+
+// let children = ["aset", "marat", "osman"];
+
+// console.log("onyn aty", name);
+// console.log("Tegi", surname);
+// console.log("neshede", age);
+// console.log("balasy", children);
+
+
+let name = "Zeiin";
+
+let surname = "Abutalib";
+
+let age = 19;
+
+let birthday = "18.06.2005" ;
+
+console.log("Aty", name);
+console.log("Tegi", surname );
+console.log("Zhasy", age);
+console.log("Tuylgan kuni", birthday);
+
+
+
+
+
+
+
+
+
+
+
